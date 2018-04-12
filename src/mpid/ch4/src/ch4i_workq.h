6b279064f4f8a0461edcc7c6c218c3b2ed1117d8
Halim Amer <aamer@anl.gov>
2018-04-12 13:59:30 -0500

    ch4: Add work queue profiling infrastructure

--- src/mpid/ch4/src/ch4i_workq.h
+++ src/mpid/ch4/src/ch4i_workq.h
@@ -10,4 +10,50 @@
 
 #include "ch4i_workq_types.h"
 
+/* For profiling */
+extern double MPIDI_pt2pt_enqueue_time;
+extern double MPIDI_pt2pt_progress_time;
+extern double MPIDI_pt2pt_issue_pend_time;
+extern double MPIDI_rma_enqueue_time;
+extern double MPIDI_rma_progress_time;
+extern double MPIDI_rma_issue_pend_time;
+
+#if defined(MPIDI_WORKQ_PROFILE)
+#define MPIDI_WORKQ_PT2PT_ENQUEUE_START  double enqueue_t1 = MPI_Wtime();
+#define MPIDI_WORKQ_PT2PT_ENQUEUE_STOP                          \
+    do {                                                        \
+        double enqueue_t2 = MPI_Wtime();                        \
+        MPIDI_pt2pt_enqueue_time += (enqueue_t2 - enqueue_t1);  \
+    } while (0)
+#define MPIDI_WORKQ_PROGRESS_START double progress_t1 = MPI_Wtime();
+#define MPIDI_WORKQ_PROGRESS_STOP                                 \
+    do {                                                                \
+        double progress_t2 = MPI_Wtime();                               \
+        MPIDI_pt2pt_progress_time += (progress_t2 - progress_t1);       \
+    } while (0)
+#define MPIDI_WORKQ_ISSUE_START    double issue_t1 = MPI_Wtime();
+#define MPIDI_WORKQ_ISSUE_STOP                            \
+    do {                                                        \
+        double issue_t2 = MPI_Wtime();                          \
+        MPIDI_pt2pt_issue_pend_time += (issue_t2 - issue_t1);   \
+    } while (0)
+
+#define MPIDI_WORKQ_RMA_ENQUEUE_START    double enqueue_t1 = MPI_Wtime();
+#define MPIDI_WORKQ_RMA_ENQUEUE_STOP                            \
+    do {                                                        \
+        double enqueue_t2 = MPI_Wtime();                        \
+        MPIDI_rma_enqueue_time += (enqueue_t2 - enqueue_t1);    \
+    } while (0)
+#else /*!defined(MPIDI_WORKQ_PROFILE) */
+#define MPIDI_WORKQ_PT2PT_ENQUEUE_START
+#define MPIDI_WORKQ_PT2PT_ENQUEUE_STOP
+#define MPIDI_WORKQ_PROGRESS_START
+#define MPIDI_WORKQ_PROGRESS_STOP
+#define MPIDI_WORKQ_ISSUE_START
+#define MPIDI_WORKQ_ISSUE_STOP
+
+#define MPIDI_WORKQ_RMA_ENQUEUE_START
+#define MPIDI_WORKQ_RMA_ENQUEUE_STOP
+#endif
+
 #endif /* CH4I_WORKQ_H_INCLUDED */
